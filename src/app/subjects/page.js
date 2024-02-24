d452aef3cc2c9d1a94097b5e9fba77ce4f8ba772
udezueoluomachi <74429592+udezueoluomachi@users.noreply.github.com>
2024-02-24 18:36:20 +0100

    Progress

--- src/app/subjects/page.js
+++ src/app/subjects/page.js
@@ -11,10 +11,29 @@ export default function Home() {
   return (
     <>
     <Header />
-    <div className="mt-20 h-2 w-full py-10">
-      <h1>
-        EXPLORE 
+    <div className="my-20 w-full p-6 md:p-16 mb-20">
+      <h1 className="text-center font-medium text-xl md:text-3xl text-black-1 my-6">
+        EXPLORE AVAILABLE SUBJECTS
       </h1>
+      <div className="px-4 py-8 h-max min-h-40 w-full bg-white-2">
+        <ul>
+            <li>
+                <a href="./physics" className="text-black-1 hover:underline" title="View topics on this subject" >
+                    Physics
+                </a>
+            </li>
+            <li>
+                <a href="./mathematics" className="text-black-1 hover:underline" title="View topics on this subject" >
+                    Mathematics
+                </a>
+            </li>
+            <li>
+                <a href="./biology" className="text-black-1 hover:underline" title="View topics on this subject" >
+                    Biology
+                </a>
+            </li>
+        </ul>
+      </div>
     </div>
     <Footer />
     </>
